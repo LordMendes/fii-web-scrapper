a8d6302c735c14014b87b1286f9ad37e01b5d91e
Lucas C Mendes <lucas.mendes@vizir.com.br>
2021-04-29 23:20:50 -0300

    change port

--- src/server.js
+++ src/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const routes = require('./shared/routes')
 
 const app = express();
-const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server runing on port: ${PORT}`)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server runing on port: ${process.env.PORT || 3000}`)
 })
