4607f5bee1e3fc164718e433a94227718e4f6b32
Lucas C Mendes <lucas.mendes@vizir.com.br>
2021-04-29 23:29:56 -0300

    change procfile

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const routes = require('./shared/routes')
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
 
+console.log(process.env.PORT)
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server runing on port: ${process.env.PORT || 3000}`)
+  console.log(`Server runing on port: ${PORT}`)
 })
