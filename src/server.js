44ce08018e62086a97af8a86a5a3e9ea25d2bc96
Lucas C Mendes <lucas.mendes@vizir.com.br>
2021-04-29 23:07:27 -0300

    add procfile

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./shared/routes')
 
 const app = express();
-const PORT = process.env.PORT ||3333;
+const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
