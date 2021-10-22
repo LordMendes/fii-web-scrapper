b36ef66c3b35d48b4ac6638c4a43fd1d418dfe83
Lucas C. Mendes <lucasmendes.lx@gmail.com>
2021-10-21 22:48:15 -0300

    feat: add gsheet route support

--- src/shared/routes/fii.routes.js
+++ src/shared/routes/fii.routes.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 const {
   fiiHeadersRequest,
   getFiiData,
-  getAllData
+  getAllData,
 } = require('../../services/requestInformation');
+const {getGSheetJson} = require('../../services/gSheetsJson')
 
 const fiiRouter = Router();
 
@@ -33,10 +34,23 @@ fiiRouter.get('/all/:fiiCode', async (request, response) => {
 
 
 fiiRouter.get('/:fiiCode', async (request, response) => {
+    console.log('ok')
+    try {
+    const { fiiCode } = request.params;
+    const fiiData = await fiiHeadersRequest(fiiCode);
+
+    return response.json(fiiData);
+  }catch(err){
+    return response.status(400).json({error: err.message})
+  }
+});
+
+
+fiiRouter.get('/gsheet/:fiiCode', async (request, response) => {
   try {
     const { fiiCode } = request.params;
 
-    const fiiData = await fiiHeadersRequest(fiiCode);
+    const fiiData = await getGSheetJson(fiiCode);
 
     return response.json(fiiData);
   }catch(err){
